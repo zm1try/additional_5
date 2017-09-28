09e991eda63afe39d2570d7c9b7ad008ab50886f
zm1try <zmitryarosh@gmail.com>
2017-09-28 22:15:02 +0300

    17/20 from home

--- src/index.js
+++ src/index.js
@@ -13,24 +13,35 @@ module.exports = function check(str, bracketsConfig) {
 
   for(i = 0; i < str.length; i++) {
     for(j = 0; j < bracketsConfig.length; j++) {
+      //if (bracketsConfig[j][0] === bracketsConfig[j][1])
       if (array[i] === bracketsConfig[j][0]) {
         stack.push(array[i]);
         l++;
+        if (stack[l-1] === array[i] && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+          stack.pop();
+          
+          l--;
+          
+        }
         //console.log(stack);
         break;
       }
       else
-        if (
-          array[i] === bracketsConfig[j][1]) {
+        if (array[i] === bracketsConfig[j][1]) {
             if ((l > 0) && (stack[l-1] === bracketsConfig[j][0])) {
               stack.pop();
               l--;
+              //console.log('fail');
               //console.log(stack);
               //console.log(l);
               
               break;
             }
-            else return false;
+            else {
+              return false;
+              //console.log('fail');
+            }
+            
         }
         else continue;
     }
